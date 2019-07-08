a836d78f7896d4d9183eb9d61061688608f9102c
Oren Me <oren.me@gmail.com>
2019-07-08 13:25:42 +0300

    fix import convention
    Co-Authored-By: Yair Ansbacher <yairans@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @flow
 import {registerPlugin} from '@playkit-js/playkit-js';
-import {Plugin} from './youbora';
+import {Youbora as Plugin} from './youbora';
 
 declare var __VERSION__: string;
 declare var __NAME__: string;
