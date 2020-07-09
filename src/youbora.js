792738c51538cbaddd1b91cffbfc06179485e8da
jesuslc91 <jesuslc.91@gmail.com>
2020-07-09 15:40:29 +0200

    external adsadapter feature

--- src/youbora.js
+++ src/youbora.js
@@ -37,7 +37,7 @@ class Youbora extends BasePlugin {
     super(name, player, config);
     this._youbora = new youbora.Plugin(this.config.options);
     this._youbora.setAdapter(new YouboraAdapter(player, config));
-    this._youbora.setAdsAdapter(new NativeAdsAdapter(player));
+    this._youbora.setAdsAdapter(config.customAdsAdapter || new NativeAdsAdapter(player));
     this._addBindings();
   }
 
@@ -52,6 +52,9 @@ class Youbora extends BasePlugin {
     this._youbora.setOptions(update.options);
     if (this._youbora.getAdapter()) {
       this._youbora.getAdapter().config = update;
+    }
+    if (update.customAdsAdapter) {
+      this._youbora.setAdsAdapter(update.customAdsAdapter);
     }
     this._addPlayerMetadata();
   }
