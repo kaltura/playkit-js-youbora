9493568d5d34db2e9a9ada61e4257fe8fd029a25
jesuslc91 <jesuslc.91@gmail.com>
2020-07-23 15:03:08 +0200

    feat(FEC-10240): external ads adapter feature, lib update (#44)
    Smartads v3 included: new ad metadata reported
    Youboralib updated to 6.7.11
    Added options to set external ads adapter
    Unit test for the new feature

--- src/adapter/ads/nativeads.js
+++ src/adapter/ads/nativeads.js
@@ -4,7 +4,7 @@ import youbora from 'youboralib';
 declare var __VERSION__: string;
 declare var __NAME__: string;
 
-let NativeAdsAdapter = youbora.StandardAdapter.extend({
+let NativeAdsAdapter = youbora.Adapter.extend({
   /**  @returns {String} - current plugin version */
   getVersion: function() {
     return youbora.VERSION + '-' + __VERSION__ + '-' + __NAME__ + '-ads';
@@ -20,6 +20,7 @@ let NativeAdsAdapter = youbora.StandardAdapter.extend({
     return this.adObject.duration;
   },
 
+  /** @returns {String} - title of the ad */
   getTitle: function() {
     return this.adObject.title;
   },
@@ -49,30 +50,101 @@ let NativeAdsAdapter = youbora.StandardAdapter.extend({
     return returnValue;
   },
 
-  /**  @returns {void} - Return a list of events and methods to suscribe from this.player. */
-  getListenersList: function() {
-    const Event = this.player.Event;
-    return [
-      {
-        events: {
-          [Event.AD_LOADED]: this.loadedAdListener,
-          [Event.AD_BREAK_START]: this.startBreakAdListener,
-          [Event.AD_STARTED]: this.startAdListener,
-          [Event.AD_RESUMED]: this.resumeAdListener,
-          [Event.AD_PAUSED]: this.pauseAdListener,
-          [Event.AD_CLICKED]: this.clickAdListener,
-          [Event.AD_SKIPPED]: this.skipAdListener,
-          [Event.AD_COMPLETED]: this.stopAdListener,
-          [Event.AD_ERROR]: this.errorAdListener,
-          [Event.AD_PROGRESS]: this.progressAdListener,
-          [Event.ALL_ADS_COMPLETED]: this.allAdsCompletedListener
-        }
+  /** @returns {Number} - the number of ad breaks included in the view */
+  getGivenBreaks: function() {
+    return this.adBreaks ? this.adBreaks.length : null;
+  },
+
+  /** @returns {Array} - a list of playheads of ad breaks begin time */
+  getBreaksTime: function() {
+    let ret = this.adBreaks;
+    if (ret) {
+      let len = ret.length;
+      if (len > 0 && ret[len - 1] === -1) {
+        ret[len - 1] = this.plugin.getDuration() || -1;
       }
-    ];
+    }
+    return ret;
+  },
+
+  /** @returns {Number} - the number of ads given for the break */
+  getGivenAds: function() {
+    return this.numAds;
+  },
+
+  /** @returns {Boolean} - if the ad is being shown in the screen or not
+   * The standard definition is: more than 50% of the pixels of the ad are on the screen
+   */
+  getIsVisible: function() {
+    return youbora.Util.calculateAdViewability(this.player.getVideoElement());
+  },
+
+  /** @returns {Boolean} - if the audio is enabled when the ad begins */
+  getAudioEnabled: function() {
+    return !this.player.muted;
+  },
+
+  /** @returns {Boolean} - if the ad is skippable */
+  getIsSkippable: function() {
+    return typeof this.adObject.skipOffset !== 'undefined' && this.adObject.skipOffset > -1;
+  },
+
+  /** @returns {Boolean} - if the player is in fullscreen mode when the ad begins */
+  getIsFullscreen: function() {
+    return this.fullscreen || false;
+  },
+
+  /** @returns {String} - ad creative id */
+  getCreativeId: function() {
+    return this.adObject.id;
+  },
+
+  /**  @returns {void} - Register listeners to this.player. */
+  registerListeners: function() {
+    const Event = this.player.Event;
+    this.references = {
+      [Event.AD_LOADED]: this.loadedAdListener.bind(this),
+      [Event.AD_STARTED]: this.startAdListener.bind(this),
+      [Event.AD_RESUMED]: this.resumeAdListener.bind(this),
+      [Event.AD_PAUSED]: this.pauseAdListener.bind(this),
+      [Event.AD_CLICKED]: this.clickAdListener.bind(this),
+      [Event.AD_SKIPPED]: this.skipAdListener.bind(this),
+      [Event.AD_COMPLETED]: this.stopAdListener.bind(this),
+      [Event.AD_ERROR]: this.errorAdListener.bind(this),
+      [Event.AD_PROGRESS]: this.progressAdListener.bind(this),
+      [Event.ALL_ADS_COMPLETED]: this.allAdsCompletedListener.bind(this),
+      [Event.AD_FIRST_QUARTILE]: this.firstQuartileListener.bind(this),
+      [Event.AD_MIDPOINT]: this.midpointListener.bind(this),
+      [Event.AD_THIRD_QUARTILE]: this.thirdQuartileListener.bind(this),
+      [Event.ENTER_FULLSCREEN]: this.enterFullscreenListener.bind(this),
+      [Event.EXIT_FULLSCREEN]: this.exitFullscreenListener.bind(this),
+      [Event.AD_MANIFEST_LOADED]: this.manifestLoaded.bind(this)
+    };
+
+    for (let key in this.references) {
+      this.player.addEventListener(key, this.references[key]);
+    }
+  },
+
+  /**  @returns {void} - Unregister listeners to this.player. */
+  unregisterListeners: function() {
+    // unregister listeners
+    if (this.player && this.references) {
+      for (let key in this.references) {
+        this.player.removeEventListener(key, this.references[key]);
+      }
+    }
+    delete this.references;
   },
 
   startBreakAdListener: function(e) {
     this.adPosition = e.payload.adBreak.type;
+    this.numAds = e.payload.adBreak.numAds;
+    this.fireBreakStart();
+  },
+
+  endBreakAdListener: function() {
+    this.fireBreakEnd();
   },
 
   loadedAdListener: function(e) {
@@ -100,11 +172,11 @@ let NativeAdsAdapter = youbora.StandardAdapter.extend({
   },
 
   clickAdListener: function() {
-    this.fireClick({adUrl: this.adObject.clickThroughUrl});
+    this.fireClick(this.adObject.clickThroughUrl);
   },
 
   skipAdListener: function() {
-    this.fireStop({skipped: true});
+    this.fireSkip();
     this.resetFlags();
   },
 
@@ -112,8 +184,34 @@ let NativeAdsAdapter = youbora.StandardAdapter.extend({
     this.fireError(e.payload.error.code, e.payload.error.message);
     if (this.getPosition() === youbora.Adapter.AdPosition.POSTROLL) {
       this.plugin.getAdapter().stopBlockedByAds = false;
-      this.plugin.getAdapter().fireStop();
+      this.plugin.fireStop();
     }
+  },
+
+  manifestLoaded: function(e) {
+    if (e && e.payload && e.payload.adBreaksPosition) {
+      this.adBreaks = e.payload.adBreaksPosition;
+    }
+  },
+
+  firstQuartileListener: function() {
+    this.fireQuartile(1);
+  },
+
+  midpointListener: function() {
+    this.fireQuartile(2);
+  },
+
+  thirdQuartileListener: function() {
+    this.fireQuartile(3);
+  },
+
+  enterFullscreenListener: function() {
+    this.fullscreen = true;
+  },
+
+  exitFullscreenListener: function() {
+    this.fullscreen = false;
   },
 
   allAdsCompletedListener: function() {
