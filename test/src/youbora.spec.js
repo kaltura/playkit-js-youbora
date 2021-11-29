8593c0f599b7ae3822261949ca27c243b65dcc7b
Yair Ansbacher <yair.ansbacher@kaltura.com>
2021-11-29 10:13:36 +0200

    feat(FEC-11725): upgrade Youbora plugin to version 6.7.42 (#80)
    upgrade youbora plugin to fix an issue with parsing manifests with relative URLs.

--- test/src/youbora.spec.js
+++ test/src/youbora.spec.js
@@ -41,7 +41,7 @@ describe('YouboraAdapter', function () {
     analyticsParams.player.should.equal(playerName);
     analyticsParams.username.should.equal(user);
     (analyticsParams.referer === document.referrer || analyticsParams.referer === location.href).should.be.true;
-    analyticsParams.pluginVersion.should.equal('6.7.35' + '-' + __VERSION__ + '-' + __NAME__);
+    analyticsParams.pluginVersion.endsWith('-' + __VERSION__ + '-' + __NAME__).should.be.true;
     analyticsParams.playerVersion.should.equal(playerVersion);
     analyticsParams.mediaResource.should.equal(resource);
     analyticsParams.mediaDuration.should.equal('13');
@@ -66,7 +66,7 @@ describe('YouboraAdapter', function () {
     analyticsParams.player.should.equal(playerName);
     analyticsParams.username.should.equal(user);
     (analyticsParams.referer === document.referrer || analyticsParams.referer === location.href).should.be.true;
-    analyticsParams.pluginVersion.should.equal('6.7.35' + '-' + __VERSION__ + '-' + __NAME__);
+    analyticsParams.pluginVersion.endsWith('-' + __VERSION__ + '-' + __NAME__).should.be.true;
     analyticsParams.playerVersion.should.equal(playerVersion);
     analyticsParams.mediaResource.should.equal(resource);
     analyticsParams.mediaDuration.should.equal('13');
